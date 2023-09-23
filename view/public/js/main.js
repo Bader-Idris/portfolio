b8b8f99ba7730adeb4abfde0ed7c96fd42b664df
Bader Idris <baidsforbusiness@gmail.com>
2023-09-23 18:29:38 +0300

    feat: landing section is done

--- view/public/js/main.js
+++ view/public/js/main.js
@@ -1,1 +1,25 @@
-const hi = 5;
+const navOptions = document.querySelectorAll('.header .main-nav>li>a');
+
+if (navOptions) {
+  navOptions.forEach((e) => {
+    // if (window.scrollY < 1000) navOptions[0].classList.add('active')
+    e.addEventListener('click', function () {
+      navOptions.forEach((el) => {
+        el.classList.remove('active');
+      });
+      e.classList.add('active');
+    });
+    if (e.classList.contains('active')) {
+      // Handle logic for elements initially marked as active
+    }
+  });
+}
+
+socialIcons = document.querySelectorAll('.social-contact .icons i');
+if (socialIcons) {
+  socialIcons.forEach(e => {
+    e.addEventListener('click', (event) => {
+      // console.log(socialIcons[0].src)
+    });
+  })
+}
