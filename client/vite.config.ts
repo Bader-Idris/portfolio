8eb188ce4aae9effbd2394e5ef5d457faebe2737
Bader Idris <baidsforbusiness@gmail.com>
2024-10-10 08:21:54 +0300

    fix: cleaned vite config file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,25 +10,10 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //! for electron 3 lines
-  //! import curDirPath, then 👇
-  // base: path.join(__dirname, "dist/"),
-//TODO: 
-  // base: "./", // Ensure compatibility with Electron
   plugins: [
     vue(),
     legacy(),
   ],
-  // plugins: [
-  //   vue({//! for using vidStack
-  //     template: {
-  //       compilerOptions: {
-  //         isCustomElement: (tag) =>
-  //           tag.startsWith("media-"),
-  //       },
-  //     },
-  //   }),
-  // ],
   css: {
     preprocessorOptions: {
       scss: {
@@ -61,56 +46,9 @@ export default defineConfig({
     include: ["@babel/preset-env", "pinia", "@capacitor/core", "vue-router"],
     exclude: ["electron"] // Electron should be excluded from Vite's optimization
   },
-  // build: {
-  //   assetsInlineLimit: 0,  // Disable inlining of font files to avoid decoding issues
-  //   rollupOptions: {
-  //     output: {
-  //       assetFileNames: 'assets/[name]-[hash][extname]'  // Ensure correct output paths and names for fonts
-  //     }
-  //   }
-  // }
-
-  /* 
-  build: {
-    minify: "terser", // Ensure minification
-    terserOptions: {
-      compress: {
-        drop_console: true, // Remove console logs for security
-        drop_debugger: true
-      }
-    },
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name]-[hash][extname]"
-      }
-    }
-  },
-  */
   test: {
     globals: true,
     environment: 'jsdom'
     // setupFiles: "vitest.setup.ts"
   },
 });
-
-
-/* prior package.json scripts 
-"scripts": {
-    "dev": "vite",
-    "build": "run-p type-check \"build-only {@}\" --",
-    "preview": "vite preview",
-    "build-only": "vite build",
-    "type-check": "vue-tsc --build --force",
-    "cypress:open": "cypress open",
-    "cypress:run": "cypress run",
-    "test:unit": "vitest",
-    "lint": "eslint . --ext .vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts --fix --ignore-path .gitignore",
-    "format": "prettier --write src/",
-    // "electron:build": "vue-tsc --noEmit && vite build && electron-builder",
-    "electron:build": "vue-tsc --noEmit && vite --config vite.electron.config.ts build && electron-builder",
-    // "electron:dev": "vite --config vite.electron.config.ts && electron .",
-    // "electron:build": "vite build --config vite.electron.config.ts && electron-builder",
-    "ionic:build": "npm run build",
-    "ionic:serve": "npm run dev -- --open"
-  },
-*/
